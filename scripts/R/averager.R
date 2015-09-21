0d763ec51098af264ed246ceab8152379acaca74
Ben Jack <bjack913@gmail.com>
2015-09-21 17:27:32 -0500

    Added script to average runs together.

---
+++ scripts/R/averager.R
@@ -0,0 +1,37 @@
+# This script averages together TABASCO simulation runs
+rm(list=ls())
+
+library(readr)
+library(stringr)
+library(dplyr)
+library(tidyr)
+
+read_mol_sim <- function(filename) {
+  # Read in file lines
+  myrun <- read_lines(filename)
+  
+  # Strip out wonky column headers that are not true columns
+  myrun[1] <- str_replace(myrun[1], '\\tNum.+', '')
+  
+  # Collapse back into string and read in as TSV
+  myrun <- str_c(myrun, collapse='\n')
+  myrun <- read_tsv(myrun)
+  
+  return(myrun)
+}
+
+sim_files <- list.files(pattern = '^.+-Mol_sim1.txt$')
+
+all_sims <- bind_rows(lapply(sim_files, read_mol_sim))
+
+avg_sims <- mutate(all_sims, `>time` = round(`>time`)) %>% # Round time points so that we can properly group the data
+  group_by(`>time`) %>%
+  summarise_each(funs(mean), -`>time`) %>% # Compute mean counts for each gene and time point
+  select(-contains('RNA')) # Select only proteins
+
+# Convert to tidy data frame for easy plotting
+avg_sims_tidy <- gather(avg_sims, gene, count, matches('*gp*'))
+myplot <- ggplot(avg_sims_tidy, aes(`>time`, count)) + 
+  geom_line() +
+  facet_wrap(~ gene) +
+  panel_border()
