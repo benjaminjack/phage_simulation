4a8a5fd658e079174288177658ad445c61c6e678
Benjamin Jack <benjamin.r.jack@gmail.com>
2016-05-06 17:32:43 -0500

    Added code for third technical replicate for Rep 5.

--- scripts/R/post_process.R
+++ scripts/R/post_process.R
@@ -40,20 +40,27 @@ normalize_areas <- function(prots) {
 
 files <- c("/Volumes/Seagate/proteomics/Rep1/Bull_11-44_9m_1a_08312014.msf", 
            "/Volumes/Seagate/proteomics/Rep1/Bull_11-44_9m_1b_08312014.msf")
-rep1 <- combine_tech_reps(files) %>% mutate(strain = '11-46', time = 9, rep = 1)
+rep1 <- combine_tech_reps(files, normalize = F, match_peps = F) %>% mutate(strain = '11-46', time = 9, rep = 1)
 rep1 <- normalize_areas(rep1)
 write_csv(rep1, "/Volumes/Seagate/proteomics/post_processed/rep1.csv")
 
 files <- c("/Volumes/Seagate/proteomics/Rep4/11-44_9min_4a_20151113.msf", 
            "/Volumes/Seagate/proteomics/Rep4/11-44_9min_4b_20151113.msf")
-rep2 <- combine_tech_reps(files) %>% mutate(strain = '11-46', time = 9, rep = 4)
+rep2 <- combine_tech_reps(files, normalize = F, match_peps = T) %>% mutate(strain = '11-46', time = 9, rep = 4)
 rep2 <- normalize_areas(rep2)
 write_csv(rep2, "/Volumes/Seagate/proteomics/post_processed/rep4.csv")
 
-files <- c("/Volumes/Seagate/proteomics/11-44_9min_5a.msf", 
-           "/Volumes/Seagate/proteomics/11-44_9min_5b.msf")
-rep3 <- combine_tech_reps(files) %>% mutate(strain = '11-46', time = 9, rep = 5)
+files <- c("/Volumes/Seagate/proteomics/Rep5/11-44_9min_5a.msf", 
+           "/Volumes/Seagate/proteomics/Rep5/11-44_9min_5b.msf",
+           "/Volumes/Seagate/proteomics/Rep5/11-44_9min_5c.msf")
+rep3 <- combine_tech_reps(files, normalize = F, match_peps = T) %>% mutate(strain = '11-46', time = 9, rep = 5)
 rep3 <- normalize_areas(rep3)
 write_csv(rep3, "/Volumes/Seagate/proteomics/post_processed/rep5.csv")
 
-all_strains <- bind_rows(rep1, rep2, rep3)
+files <- c("/Volumes/Seagate/proteomics/Rep2/11-44_9min_1a_20150512.msf", 
+           "/Volumes/Seagate/proteomics/Rep2/11-44_9min_1b_20150512.msf")
+rep4 <- combine_tech_reps(files, normalize = F, match_peps = T) %>% mutate(strain = '11-46', time = 9, rep = 2)
+rep4 <- normalize_areas(rep4)
+write_csv(rep3, "/Volumes/Seagate/proteomics/post_processed/rep2.csv")
+
+all_strains <- bind_rows(rep1, rep2, rep3, rep4)
