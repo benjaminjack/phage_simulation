2a5ffe05573724327e0bc1de734aeb85fddf560c
benjaminjack <bjack913@gmail.com>
2017-08-14 20:47:52 -0500

    new command lsit

--- commands.sh
+++ commands.sh
@@ -1,4 +1,1 @@
-pinetree_run.py ./params/T7_081417_rec.yml -o ./runs/run_081417_rec
-pinetree_run.py ./params/T7_081417_wt.yml -o ./runs/run_081417_wt
-pinetree_run.py ./params/T7_081417b_rec.yml -o ./runs/run_081417b_rec
-pinetree_run.py ./params/T7_081417b_wt.yml -o ./runs/run_081417b_wt
+pinetree_run.py ./params/T7_081417c_wt.yml -o ./runs/run_081417c_wt
