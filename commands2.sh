3513564d338baf687d1a184fdcc05f18749637b3
benjaminjack <bjack913@gmail.com>
2018-05-10 17:17:14 -0500

    added new rna-seq data

--- commands2.sh
+++
@@ -1,4 +0,0 @@
-pinetree_run.py ./params/T7_081617_wt.yml -o ./runs/run_081617_wt
-pinetree_run.py ./params/T7_081617_rec.yml -o ./runs/run_081617_rec
-pinetree_run.py ./params/T7_081617b_wt.yml -o ./runs/run_081617b_wt
-pinetree_run.py ./params/T7_081617b_rec.yml -o ./runs/run_081617b_rec
